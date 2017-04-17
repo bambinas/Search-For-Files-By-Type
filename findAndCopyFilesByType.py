8ecf636327b5a92c70e6e162524f72830fa72136
bambinas <rudolfas.lamsargis@gmail.com>
2017-04-17 17:46:12 +0300

    V2
    Added functionality that lets user to enter the type of extension to
    search for.

--- findAndCopyFilesByType.py
+++ findAndCopyFilesByType.py
@@ -4,21 +4,23 @@
 # location they are in to a new folder.
 
 import os, shutil
-# TODO: extension we are searhing for:
-extension = '.pdf'
-# TODO: set a path, where files with certain extensions will be searched for.
+# let user choose what extension they want to search for:
+print('Enter extension type you want to search for:')
+extension = input()
+# set a path, where files with certain extensions will be searched for.
 location = 'C:\\Users\\rudol\\Dropbox\\HUMF'
-# TODO: set a path, where found files will be copied to.
+# set a path, where found files will be copied to.
 destination = 'C:\\FilesFound'
-# TODO: walk the folder tree.
+# walk the folder tree.
 for foldername, subfolders, filenames in os.walk(location):
 	for filename in filenames:
-		# TODO: check if each file is with required extension.
+		# check if each file is with required extension.
 		if filename.endswith(extension):
+			# get the absolute path of found file.
 			filename = os.path.join(foldername, filename)
-			# TODO: print found file to the screen.
+			# print found file to the screen.
 			print ('Found ' + "'" + os.path.basename(filename) + "'" + ' in ' +  "'" + os.path.dirname(filename) + "'" + '. ' + 'File copied to ' + "'" + destination +   "'" +'.')
-			# TODO: copy file to the path.
+			# copy file to the path.
 			shutil.copy(filename, destination)
 
 print('All files with ' + extension + ' extension were copied.')
